32e095b0be15ee66b4016c48d1ac4f1d50a5dc6f
nokeludak <ksmajic996@gmail.com>
2021-03-23 02:47:09 +0100

    Ispravka ex08

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,13 +1,8 @@
 // Only change code below this line
-const sum = (num1, num2 = 1) => num1 + num2; {
-    if (num2 = 0){
-        sum = num1 + 1;
-
-    return sum;
-    };
-    
-}
-// Only change vode above this line
-console.log(sum(4, 6));
-console.log(sum(3));
-module.exports = sum;
+const sum = (num1, num2 = 1) =>  {
+    return num1 + num2; 
+ };
+ // Only change code above this line
+ console.log(sum(4, 6)); // Change this line
+ console.log(sum(3));
+ module.exports = sum;
